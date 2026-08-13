47b7dd222ba8c8d3ca1559f0ded8a9235647c92e
Michael <y.liu@camari-international.com>
2026-08-13 16:21:38 +0800

    Restore linting for Next.js 16

---
+++ eslint.config.mjs
@@ -0,0 +1,25 @@
+import { defineConfig, globalIgnores } from "eslint/config";
+import nextVitals from "eslint-config-next/core-web-vitals";
+import nextTypeScript from "eslint-config-next/typescript";
+
+export default defineConfig([
+  ...nextVitals,
+  ...nextTypeScript,
+  {
+    rules: {
+      "react-hooks/immutability": "off",
+      "react-hooks/set-state-in-effect": "off",
+      "react-hooks/use-memo": "off",
+    },
+  },
+  globalIgnores([
+    ".codex-tmp/**",
+    ".codex-work/**",
+    ".next/**",
+    ".open-next/**",
+    ".wrangler/**",
+    "dist/**",
+    "next-env.d.ts",
+    "public/**",
+  ]),
+]);
